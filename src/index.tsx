d2cafa85e0cfb8f893e9f684bddf36ae220ab6d7
Mykola Lysenko <niels.kaizenman@gmail.com>
2022-11-11 01:00:42 +0200

    experiments with compiler-explorer API

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Editor } from './components/Editor';
+import { Console } from "./components/Console";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Editor />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <div className="Container">
+      <Editor />
+      <Console />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
